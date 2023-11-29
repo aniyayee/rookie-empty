dd4f6e0df5f6b633f661cc0fdf6aaad8bf0cceaf
yayee <code.alpaca@qq.com>
2023-11-29 11:04:06 +0800

    feat: Excel文件处理

---
+++ src/main/java/com/rookie/customize/poi/ExcelSheet.java
@@ -0,0 +1,19 @@
+package com.rookie.customize.poi;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @author yayee
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface ExcelSheet {
+
+    /**
+     * sheet名称
+     */
+    String name() default "";
+}
---
+++ src/test/java/com/rookie/utils/poi/PostDTO.java
@@ -0,0 +1,40 @@
+package com.rookie.utils.poi;
+
+import com.rookie.customize.poi.ExcelColumn;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author yayee
+ */
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@EqualsAndHashCode
+public class PostDTO {
+
+    @ExcelColumn(name = "岗位ID")
+    private Long postId;
+
+
+    @ExcelColumn(name = "岗位编码")
+    private String postCode;
+
+    @ExcelColumn(name = "岗位名称")
+    private String postName;
+
+
+    @ExcelColumn(name = "岗位排序")
+    private String postSort;
+
+    @ExcelColumn(name = "备注")
+    private String remark;
+
+    private String status;
+
+    @ExcelColumn(name = "状态")
+    private String statusStr;
+}
+
